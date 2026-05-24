74a2be3ec41fa149a408c35ff1e7e507a8f77e14
aakriti0223 <aakrititandon211@gmail.com>
2026-05-24 16:09:40 +0530

    Update cookie settings for secure authentication

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -16,8 +16,8 @@ export const googleAuth=async (req, res)=>{
 
         res.cookie("token", token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"strict",
+            secure:true,
+            sameSite:"none",
             maxAge:7*24*60*60*1000
         })
 
@@ -33,8 +33,8 @@ export const logOut=async (req, res)=>{
     try{
         res.clearCookie("token",{
             httpOnly:true,
-            secure:false,
-            sameSite:"strict"
+            secure:true,
+            sameSite:"none"
         })
         return res.status(200).json({message:"Logged out successfully"})
 } catch(error){
